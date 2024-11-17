4f58c2e628ffa469084fc76b895655f45f316703
unknown <marufuzzaman7@gmail.com>
2024-11-17 23:04:42 +0600

    Update the code

--- test/upload-video.js
+++ test/upload-video.js
@@ -6,22 +6,35 @@ describe('Upload Video by provider', function() {
     it('Video upload', function(browser) {
         commonFunctions.Login(browser);
         browser
+        //Click on Upload video button
         .click('xpath', '//*[@id="header-nav"]/div[2]/div[2]/div[1]/a')
-        .waitForElementVisible('body')
+        //.waitForElementVisible('body')
+       // .pause(10000)
         //Enter video title
-        .setValue('xpath', '//*[@id="videotitle"]', 'Demo Video')
-        //Ente video description
-        .setValue('xpath', '/html/body/main/section/div/div/div[1]/div/div[2]/div[2]/div/div[2]/div[1]', 'Demo Description')
-        //Enter tag
-        .setValue('xpath', '/html/body/main/section/div/div/div[1]/div/div[3]/div[4]/div[1]/div[1]/input', 'test1')
+        //.setValue('xpath', '//*[@id="videotitle"]', 'Demo Video')
+        //Enter video description
+        //.setValue('xpath', '/html/body/main/section/div/div/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/p', 'Demo Description')
+        //Enter tags
+
+        .waitForElementVisible('input[type="text"]', 1000)  
+        .setValue('xpath', '/html/body/main/section/div/div/div[1]/div/div[3]/div[4]/div[1]/div[1]/input', 'islamic gojol') 
+        
+        //.setValue('xpath', '/html/body/main/section/div/div/div[1]/div/div[3]/div[4]/div[1]/div[1]/input', 'islamic gojol', [browser.Keys.ENTER])
+        //.setValue('input[name="fieldName"]', [browser.Keys.ENTER])
+        //.pause(5000)
+        //.click("//*[contains(text(), 'islamic gojol')]")
+        //.click('xpath', '/html/body/main/section/div/div/div[1]/div/div[3]/div[4]/div[1]/div[1]/input')
         
         //.setValue('xpath', '/html/body/main/section/div/div/div[1]/div/div[3]/div[4]/div[1]/div[1]/input', 'test2')
         
         //.setValue('xpath', '/html/body/main/section/div/div/div[1]/div/div[3]/div[4]/div[1]/div[1]/input', 'test3')
-        .waitForElementVisible('xpath','/html/body/main/section/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div/div[2]/div[1]/button', 1000) 
-        .uploadFile('xpath','/html/body/main/section/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div/div[2]/div[1]/button', 'C:\Users\MARUF\Downloads\file_example_MP4_480_1_5MG.mp4') 
-        .pause(1000)
-        
+       // .waitForElementVisible('xpath','/html/body/main/section/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div/div[2]/div[1]/button', 1000) 
+        //.click('xpath', '/html/body/main/section/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div/div[2]/div[1]/button')
+        //.uploadFile('input[class="uppy-Dashboard-input"]', 'C:/Users/MARUF/Downloads/file_example_MP4_480_1_5MG.mp4') 
+        //.pause(5000)
+       // .click('xpath', '/html/body/main/section/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div/div[4]/div[1]/div[2]/button')
+       // .uploadFile('input[id="uploadmythumb"]', 'C:/Users/MARUF/Downloads/getty-us-network-map-384x216.jpg') 
+      //  .pause(5000)
         
     });
   
